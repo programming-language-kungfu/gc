7c978607b561b58f2561a34f9c7e5a1d7373301e
Kisitu Augustine <austiine04@gmail.com>
2014-07-02 16:38:00 +0300

    a basic vm

---
+++ vm.c
@@ -0,0 +1,44 @@
+#include "types.h"
+#define STACK_SIZE 256
+
+typedef struct {
+    Object* stack[STACK_MAX];
+    int stack_size;
+} VM;
+
+VM * new_vm () {
+    VM * vm = malloc(sizeof(VM));
+    vm->stack_size = 0;
+    return vm;
+}
+
+void push (VM* vm, Object* value) {
+    assert(vm->stack_size < STACK_SIZE, "Stack Overflow");
+    vm->stack[vm->stack_size++] = value;
+}
+
+Object* pop (VM* vm) {
+    assert(vm->stack[--vm->stack_size] > 0, "Stack Underflow");
+    return vm->stack[--vm->stack_size];
+}
+
+Object* new_object (ObjectType type) {
+    Object* object = malloc(sizeof(Object));
+    object->type = type;
+    return object;
+}
+
+void push_in (VM* vm, int int_value) {
+    Object* object = new_object(OBJ_INT);
+    object->value = int_value;
+    push(vm, object);
+}
+
+Object* push_pair(VM* vm) {
+    Object* object = new_object(OBJ_PAIR);
+    object->tail = pop(vm);
+    object->head = pop(vm);
+
+    push(vm ,object);
+    return object;
+}
