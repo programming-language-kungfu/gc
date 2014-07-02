e6abe97babc3fcdbcc73e008cc0a1dffd9560fe8
Kisitu Augustine <austiine04@gmail.com>
2014-07-02 15:43:00 +0300

    basic types for the vm

---
+++ types.h
@@ -0,0 +1,24 @@
+/**
+ * interpreter has two data types ie an int and a pair,
+ * an object in the language of this interpreter can be an int or a pair.
+ */
+
+typedef enum {
+    OBJ_INT,
+    OBJ_PAIR
+} ObjectType;
+
+typedef struct sObject {
+    ObjectType type;
+
+    union {
+        /* OBJ_INT */
+        int value;
+
+        /* OBJ_PAIR */
+        struct {
+            struct sObject* head;
+            struct sObject* tail;
+        };
+    };
+} Object;
